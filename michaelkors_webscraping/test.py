e55305fbd6b458abd86e3dea5f6e66e06bda02b8
Samitha99 <73548027+Samitha99@users.noreply.github.com>
2022-10-23 16:29:16 +0530

    Update test.py

--- michaelkors_webscraping/test.py
+++ michaelkors_webscraping/test.py
@@ -1,6 +1,24 @@
 import requests
 from bs4 import BeautifulSoup
 
-soures = requests.get('https://www.michaelkors.com/sale/men/_/N-28au')
-soures.raise_for_status
-print(soures)
+try:
+    soures = requests.get('https://www.michaelkors.com/sale/men/_/N-28au')
+    soures.raise_for_status
+
+    soup = BeautifulSoup(soures.text, 'html.parser')
+    products = soup.find_all('li', class_="product-tile left small-6 medium-3")
+    print(len(products))
+
+    n = 1
+    for product in products:
+        productNumber = n
+        productName = product.find('li', class_="product-name-container").a.text.strip()
+        productPrice = product.find('div', class_="salePrice").find('span', class_="ada-link visually-hidden").text.strip()
+        productOldPrice = product.find('div', class_="listPrice").find('span', class_="ada-link visually-hidden").text.strip()
+        print(productNumber, productName, productPrice, productOldPrice)
+        n+=1
+
+
+except Exception as e:
+    print(e)
+    exit()
